e156ce124c17df961ad8db69105b0e767e8e48e6
Pratik <pratikaambani@gmail.com>
2020-07-17 01:45:36 +0530

    adds eureka client running on https

---
+++ src/main/java/com/practise/eureka/SecureClientApplication.java
@@ -0,0 +1,13 @@
+package com.practise.eureka;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class SecureClientApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(SecureClientApplication.class).run(args);
+    }
+
+}
